b4ca3b49395c4e41803168c3f377d385a7a715b4
joaom95 <45365912+joaom95@users.noreply.github.com>
2019-11-20 01:28:55 +0000

    Update ex1.js

--- p5js/ex1.js
+++ p5js/ex1.js
@@ -1,20 +1,52 @@
 //Variáveis tipo let nomeDaVar;
+let posX, posY;
+let velX, velY;
+let padWidth, padHeight;
+let diamBall;
+let points;
+let vel;
 
-function setup() { //código que só corre uma vez
-
-  ///Não Apagar para Baixo até <<<
-  var canvasDiv = document.getElementById('ex1');
-  var width = canvasDiv.offsetWidth;
-  var sketchCanvas = createCanvas(width,width*.75);
-  console.log(sketchCanvas);
-  sketchCanvas.parent('ex1');
-  ///<<< Não fica a faltar createCanvas
-
-
+function setup() {
+  createCanvas(300, 200);
+  posX = random(width);
+  posY = 30;
+  vel = 5;
+  velX = vel;
+  velY = vel;
+  padWidth = 80;
+  padHeight = 8;
+  diamBall = 15;
+  points=0;
 }
 
-function draw() { //código que corre em loop
-  background(220); //podem apagar esta linha!
+function draw() {
+  //desenha
+  background(220);
+  text(points,20,20);
+  posX = posX + velX;
+  posY = posY + velY;
+  ellipse(posX,posY,diamBall);
+  rectMode(CENTER);
+  rect(mouseX,height-10,padWidth,padHeight);
 
+  //calcula
+  if (posX-diamBall/2 <= 0 || posX+diamBall/2 >= width) {
+    velX = velX*-1;
+  }
+  if (posY-diamBall/2 <= 0) {
+    velY = velY*-1;
+  }
 
+  if (posY+diamBall/2 >= height){
+    print("perdeste");
+    velX = 0;
+    velY = 0;
+    points = "perdeste";
+  }
+
+  if (velX != 0 && posX >= mouseX-padWidth/2 && posX <= mouseX+padWidth/2 && posY+diamBall/2 >= height-10-padHeight/2){
+    velY = -random(vel-3,vel);
+    velX =random(-vel,vel);
+    points++;
+  }
 }
